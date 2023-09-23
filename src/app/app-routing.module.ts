9450183938763a6b2f7019a8c04124cfe995105b
DouglasGeovanne01 <douglas.geovanne01@gmail.com>
2023-09-22 23:32:28 -0300

    Ajuste rota pag inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,7 +58,7 @@ const routes: Routes = [
         path: 'loginEmpresa',
         component: LoginEmpresaComponent
     }, {
-        path: 'paginaInicialEmpresa',
+        path: 'paginaInicialEmpresa/:id',
         component: PaginaInicialEmpresaComponent
     },
     // {
--- src/app/componentes/empresas/pagina-inicial-empresa/pagina-inicial-empresa.component.ts
+++ src/app/componentes/empresas/pagina-inicial-empresa/pagina-inicial-empresa.component.ts
@@ -1,19 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VagasService } from '../../vagas/vagas.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmpresasService } from '../empresas.service';
+import { Empresas } from '../empresas';
 
 @Component({
   selector: 'app-pagina-inicial-empresa',
   templateUrl: './pagina-inicial-empresa.component.html',
   styleUrls: ['./pagina-inicial-empresa.component.css']
 })
-export class PaginaInicialEmpresaComponent {
+export class PaginaInicialEmpresaComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private route : ActivatedRoute,
+    private service : EmpresasService
     ){
 
   }
+
+  empresa:Empresas={
+    id:0,
+    cnpj:'',
+    razaoSocial:'',
+    nomeFantasia:'',
+    endereco:{
+      cep:'',
+      rua:'',
+      numero:0,
+      cidade:'',
+      estado:''    
+    },
+    email:'',
+    senha:'',
+    linkedin:'',
+    siteInstitucional:''
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+        this.service.buscarPorId(parseInt(id)).subscribe((empresa) => {
+            this.empresa = empresa;
+        });
+    }
+}
 
   listarVagas(){
     this.router.navigate(['/listarVagas'])
@@ -22,5 +53,11 @@ export class PaginaInicialEmpresaComponent {
   listarCandidatos() {
     this.router.navigate(['/listarCandidato'])
   }
+
+//   editarEmpresa() {
+//     this.service.editar(this.empresa).subscribe(() => {
+//         this.router.navigate(['/listarCandidato'])
+//     })
+// }
 
 }
