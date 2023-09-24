89c36e5158303acb7954699148a0df4cdf0cf53b
DouglasGeovanne01 <douglas.geovanne01@gmail.com>
2023-09-24 13:34:56 -0300

    Indice candidatos cadastrados

--- src/app/componentes/everymind/dashboard/dashboard.component.ts
+++ src/app/componentes/everymind/dashboard/dashboard.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { CandidatoService } from '../../candidatos/candidato.service';
+import { Candidato } from '../../candidatos/candidato';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  listaCandidatos:Candidato[]=[]
+
+  ngOnInit(){
+    
+    this.candidatoService.listar().subscribe((listaCandidatos) => {
+      this.listaCandidatos = listaCandidatos
+      
+    })
+
+    
+  }
   
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -37,6 +50,6 @@ export class DashboardComponent {
     );
 
   
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private candidatoService : CandidatoService) {}
 
 }
