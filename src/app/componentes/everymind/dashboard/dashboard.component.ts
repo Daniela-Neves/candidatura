8c5c8bbb500d8c0e2e5ad96bdadfac165bbad8df
Loen <luantgaion562@gmail.com>
2023-09-24 18:01:09 -0300

    menu-em BONITO

--- src/app/componentes/everymind/dashboard/dashboard.component.ts
+++ src/app/componentes/everymind/dashboard/dashboard.component.ts
@@ -35,34 +35,7 @@ export class DashboardComponent implements OnInit{
     })
     
   }
-  
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-       ];
-      }
-  
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );    
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  
   constructor(private breakpointObserver: BreakpointObserver, private candidatoService : CandidatoService, 
     private empresaService:EmpresasService, private vagasService:VagasService) {}
 
